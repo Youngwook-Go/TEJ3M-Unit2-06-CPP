0976bb4e11cea349d3b55103bdcce970b5bc8ad1
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-10-17 15:15:35 +0000

    m

---
+++ distance.cpp
@@ -0,0 +1,30 @@
+// Created by: Youngwook Go
+// Created on: OCT 2023
+// Distance sensor
+
+
+const int trigPin = 3;
+const int echoPin = 2;
+float duration;
+float distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);  
+  pinMode(echoPin, INPUT);  
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2);  
+  digitalWrite(trigPin, HIGH);  
+  delayMicroseconds(10);  
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+
+  Serial.print("Distance: ");  
+  Serial.println(distance);  
+  delay(100);
+}
